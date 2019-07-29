b11beb58b316d28c37f91ca84be0881107dc5b9a
myname21-meet <myname21@meet.mit.edu>
2019-07-29 10:05:32 -0400

    afaf

--- knowledge_databases.py
+++ knowledge_databases.py
@@ -1,4 +1,4 @@
-from knowledge_model import Base, Knowledge
+from knowledge_model import Base, Knowledge, Website
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,20 +8,49 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(topic, title, rating, host_site_id):
+	article_object = Knowledge(
+		topic=topic,
+		title=title,
+		rating=rating,
+		host_site_id=host_site_id)
+	session.add(article_object)
+	session.commit()
+
+def add_site(articles_num, name, rating):
+	site_object = Website(
+		articles_num=articles_num,
+		name=name,
+		rating=rating)
+	session.add(site_object)
+	session.commit()
 
 def query_all_articles():
-	pass
+	articles = session.query(Knowledge).all()
+	return articles
 
-def query_article_by_topic():
-	pass
+def query_article_by_topic(topic):
+	article = session.query(Knowledge).filter_by(
+		topic=topic).first()
+	return article
 
-def delete_article_by_topic():
-	pass
+def delete_article_by_topic(topic):
+	session.query(Knowledge).filter_by(
+		topic=topic).delete()
+	session.commit()
 
 def delete_all_articles():
-	pass
+	session.query(Knowledge).delete()
+	session.commit()
 
-def edit_article_rating():
-	pass
+def edit_article_rating(title, rating):
+	article_object = session.query(
+		Knowledge).filter_by(
+		title=title).first()
+	article_object.rating = rating
+	session.commit()
+
+delete_all_articles()
+
+add_article("Computer Science", "Foreign Key", 8, 1)
+print(query_article_by_topic("Computer Science").host_site_id)
--- knowledge_model.py
+++ knowledge_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
@@ -13,5 +13,24 @@ class Knowledge(Base):
 	# The third column will be a string representing the 
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
+	__tablename__ = 'articles'
+	article_num = Column(Integer, primary_key=True)
+	topic = Column(String)
+	title = Column(String)
+	rating = Column(Integer)
 
-	pass
+	host_site = relationship("Website")
+	host_site_id = Column(Integer, ForeignKey("website.site_id"))
+class Website(Base):
+	# Create a table with 4 columns
+	# The first column will be the primary key
+	# The second column should be a string representing
+	# the name of the Wiki article that you're referencing
+	# The third column will be a string representing the 
+	# topic of the article. The last column will be
+	# an integer, representing your rating of the article.
+	__tablename__ = 'website'
+	site_id = Column(Integer, primary_key=True)
+	articles_num = Column(Integer)
+	name = Column(String)
+	rating = Column(Integer)
